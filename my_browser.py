7fa7ea7c0267553698ca4233afcf1c9657e45447
snehasighakolli <130784486+snehasighakolli@users.noreply.github.com>
2024-08-11 01:21:39 +0530

    Add files via upload

---
+++ my_browser.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 19 13:31:57 2023
+
+@author: sneha
+"""
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QToolBar, QAction, QLineEdit
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('https://google.com'))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+        # navbar
+        navbar = QToolBar()
+        self.addToolBar(navbar)
+
+        back_btn = QAction('Back', self)
+        back_btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_btn)
+
+        forward_btn = QAction('Forward', self)
+        forward_btn.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_btn)
+
+        reload_btn = QAction('Reload', self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        home_btn = QAction('Home', self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
+
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
+
+        self.browser.urlChanged.connect(self.update_url)
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl('https://sighakollisneha.com'))
+
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName("SIGHAKOLLI SNEHA's browser")
+window = MainWindow()
+app.exec_()
